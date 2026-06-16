63cd91fd0ba97a13ddc3e28a22b6255668b2c3a3
Eizo0810 <secondnature5303@leaf.ocn.ne.jp>
2026-06-16 09:33:29 +0900

    Initial Spring Boot project

---
+++ src/test/java/com/example/attendancemanager/AttendancemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attendancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendancemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
